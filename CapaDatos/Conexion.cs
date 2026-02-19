fbf3145333f3766f3c39ad560e8f446ece95e567
jesus <jesus@YANIXLTW>
2026-02-19 15:38:24 -0700

    Agregar archivos de proyecto.

---
+++ CapaDatos/Conexion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    internal class Conexion
+    {
+        public static string Conn = "Data source = localhost; Initial Catalog = dbventas; Integrated Security = true";
+    }
+}
